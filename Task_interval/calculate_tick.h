8e10a384ecb8a5c7d42e6a548de73758651dfcdf
junhua-best <55447079+junhua-best@users.noreply.github.com>
2021-06-10 10:27:01 +0800

    Create calculate_tick.h

---
+++ Task_interval/calculate_tick.h
@@ -0,0 +1,16 @@
+
+#ifndef CALCULATE_TICK_H
+#define CALCULATE_TICK_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+typedef struct time_interval
+{
+    unsigned int  interval; //time interval
+};
+
+
+#endif
